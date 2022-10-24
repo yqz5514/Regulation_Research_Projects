4afdc0d57917929907a4b7a3158aa2c2f7d50308
Yaxin(Janet) Zhuang <49347418+yqz5514@users.noreply.github.com>
2022-10-24 15:29:16 -0400

    Update task1.py

--- Other_projects/task1.py
+++ Other_projects/task1.py
@@ -4,3 +4,22 @@ import pandas as pd
 df = pd.read_csv('org.csv')
 # %%
 df.head()
+# %%
+df.columns
+# %%
+df1 = df[['documentId','title']]
+# %%
+df1.head()
+# %%
+df1['org'] = df1['title']
+# %%
+df1
+# %%
+df1['org'] = df1['org'].str[21:]
+# %%
+df1.org.values
+# %%
+new = df1['org'].str.split(",", n = 3, expand = True)
+#%%
+new
+# %%
